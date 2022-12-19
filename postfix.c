4bcb6dd44558953a614b0b59cd588a1b43eb2bfb
21A91A05C5 <97424467+21A91A05C5@users.noreply.github.com>
2022-12-19 21:02:05 +0530

    Add files via upload

---
+++ postfix.c
@@ -0,0 +1,61 @@
+#include<stdio.h>
+char post[100];
+int st[100],top=-1;
+int isop(char ch)
+{
+	switch(ch)
+	{
+		case'+':return 1;
+		case'-':return 1;
+		case'*':return 2;
+		case'/':return 2;
+		default :return 0;
+	}
+}
+int operation(int op2,char op,int op1)
+{
+	switch(op)
+	{
+		case'+':return op2+op1;
+		case'-':return op2-op1;
+		case'*':return op2*op1;
+		case'/':return op2/op1;
+	}
+}
+
+void post_eval(char*str)
+{
+	int i,op1,op2,k=0,val;
+	char temp[100];
+	for(i=0;str[i]!='\0';i++)
+	{
+		if(isop(str[i]))
+		{
+			op1=st[top--];
+			op2=st[top--];
+			st[++top]=operation(op2,str[i],op1);
+		}
+		else
+		{
+			if(str[i]!=' ')
+			{
+				temp[k++]=str[i];
+			}
+			else if(k!=0)
+			{
+				temp[k]='\0';
+				val=atoi(temp);
+				st[++top]=val;
+				k=0;
+			}
+		}
+	}
+}
+void main()
+{
+	scanf("%[^\n]s",&post);
+	post_eval(post);
+	printf("%d",st[0]);
+}
+
+
